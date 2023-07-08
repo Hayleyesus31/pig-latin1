931f3b8a3532e334fbba88d929b70e8d5380bb74
Replit user <>
2023-07-08 02:12:06 +0000

    pig latin

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function translatePigLatin(word) {
+ 
+  if (!word || word.length < 2) {
+    return `The input cannot be translated.`;
+  }
+
+  let firstLetter = word[0];
+
+  const vowels = ["a", "e", "i", "o", "u"];
+  if (vowels.includes(firstLetter)) {
+    return `${word}way`;
+  }
+
+  return `${word.slice(1)}${firstLetter}ay`;
+}
+
+let input = prompt("Enter a word or phrase:");
+
+let pigLatin = translatePigLatin(input);
+
+console.log(pigLatin);
 
